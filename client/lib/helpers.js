25158a137a3f5da491080b123f9133877104792a
Mike Barkas <mike@barkas.com>
2015-05-18 21:00:44 -0400

    Switch statement for alert types.

--- client/lib/helpers.js
+++ client/lib/helpers.js
@@ -1,4 +1,41 @@
 // Meteor Set Message.
 // Global variable to display a message in templates.
-setMessage = function (type, message) {
+setMessage = function (message = NULL, type = NULL) {
+  if (message) {
+    // Conditional check for success, warning, or danger.
+    switch  (type) {
+      // Define alerts with bootstrap classes.
+      case 'success':
+      var alert = '<div class="alert alert-success alert-dismissible" role="alert">
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>'
+         + message +
+        '</div>';
+      break;
+
+      case 'warning':
+      var alert = '<div class="alert alert-warning alert-dismissible" role="alert">
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>'
+         + message +
+        '</div>';
+      break;
+
+      case 'danger':
+      var alert = '<div class="alert alert-danger alert-dismissible" role="alert">
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>'
+         + message +
+        '</div>';
+      break;
+
+      default:
+      var alert = undefined;
+      break;
+    }
+    return alert;
+  }
 }
